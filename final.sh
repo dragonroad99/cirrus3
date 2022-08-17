f8c2fc75e392f64c13a9a4ad483612f69ca7a0fb
dragonroad99 <fadhilm298@gmail.com>
2022-08-17 20:44:09 +0000

    initial bootleg

--- final.sh
+++ final.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-cd /tmp/rom
-
-export CCACHE_DIR=/tmp/ccache
-export CCACHE_EXEC=$(which ccache)
-export USE_CCACHE=1
-ccache -M 20G
-ccache -o compression=true
-ccache -z
+cd ~/rom
 
 . build/envsetup.sh
-lunch nad_juice-userdebug
-mka nad -j8
+export CCACHE_DIR=~/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 20G -F 0
+ccache -o compression=true
+ccache -z
+export BUILD_USERNAME="darknius"
+export BUILD_HOSTNAME="darx-labs"
+$lunch
+breakfast juice
+$make -j8
