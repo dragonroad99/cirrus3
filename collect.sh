4eca52a54160608c8c89147e555f78f77f148948
dragonroad99 <fadhilm298@gmail.com>
2022-08-18 19:31:04 +0700

    Try build some file before build rom

--- collect.sh
+++ collect.sh
@@ -9,6 +9,11 @@ ccache -M 20G -F 0
 export BUILD_USERNAME="darknius"
 export BUILD_HOSTNAME="darx"
 $lunch
+mka api-stubs-docs
+mka hiddenapi-lists-docs
+mka system-api-stubs-docs
+mka test-api-stubs-docs
+mka kronic
 breakfast juice
 $make -j8 &
 sleep 100m
