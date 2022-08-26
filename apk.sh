7650b9588716a216c1a92c9d97351a1fa0b08f15
dragonroad99 <fadhilm298@gmail.com>
2022-08-26 12:19:46 +0000

    add build separated apk

---
+++ apk.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ~/rom
+
+. build/envsetup.sh
+export CCACHE_DIR=~/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 20G -F 0
+export BUILD_USERNAME="darknius"
+export BUILD_HOSTNAME="darx-labs"
+$lunch
+export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
+export BUILD_BROKEN_DUP_RULES=true
+make SystemUI -j8
